d959ac5ec2f1a1a74fcd7132251fb89b300c0731
moon <348451742@qq.com>
2023-09-18 01:29:14 +0800

    引入Color类

--- src/ploy/vehicle/Mini.java
+++ src/ploy/vehicle/Mini.java
@@ -4,32 +4,33 @@ package ploy.vehicle;
 /*
 
  */
+import java.awt.Color;
+
 public class Mini extends Car{
     
-    private int color;
+    private Color color;
 
-    public Mini(int c){
+    public Mini(Color c){
         super("Mini");
         color = c;
     }
 
     public Mini(){
-        // 暂时110表示红色
-        this(110);
+        this(Color.RED);
     }
 
     
     /*  
      * 由于super和this都必须定义在构造函数的第一行，因此下面写法不合法，可以改成：
     public Mini(String size){
-         this(110);
+         this(Color.RED);
          super(size);
     }
     */
 
     public Mini(String size){
         super(size);
-        color = 110;
+        color = Color.RED;
     }
 
     
